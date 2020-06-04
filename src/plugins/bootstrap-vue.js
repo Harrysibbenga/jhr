e74039d9e0d8de1c652fdcfe264eea85bddea427
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-04 13:51:27 +0100

    updated bootstrap pakage

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 
-import Bootstrap from "bootstrap";
+import Bootstrap from "bootstrap-vue";
 import "../../node_modules/bootstrap/scss/bootstrap.scss";
 
 Vue.use(Bootstrap);
