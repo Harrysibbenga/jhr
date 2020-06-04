570d1bc15e6361eca4326b52b291db77210acec4
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-04 13:51:59 +0100

    imported bootstrap plugin and scss file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/bootstrap-vue.js'
+import './assets/scss/custom.scss'
 
 Vue.config.productionTip = false
 
