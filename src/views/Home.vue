9a146039f34f73214098887efb84f17b0a6695a4
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-10 16:06:05 +0100

    Added the header to the home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="home">
+    <Header></Header>
     <carousel></carousel>
     <latest></latest>
     <calendar></calendar>
@@ -11,13 +12,22 @@
 import Carousel from "@/components/Carousel.vue";
 import Latest from "@/components/Latest.vue";
 import Calendar from "@/components/Calendar.vue";
+import Header from "@/components/Header-footer/Header";
 
 export default {
   name: "Home",
   components: {
     Carousel,
     Latest,
-    Calendar
+    Calendar,
+    Header
   }
 };
 </script>
+
+<style scoped>
+/* header {
+  z-index: 2;
+  color: white !important;
+} */
+</style>
