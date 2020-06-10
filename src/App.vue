b34e02eb35c407ef76f7795502aed7cafc7f45b7
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-10 16:04:37 +0100

    Moved and updated th header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
-    <Header></Header>
     <router-view />
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from "@/components/Header-footer/Header";
 import Footer from "@/components/Header-footer/Footer";
 
 export default {
   components: {
-    Header,
-    Footer,
-  },
+    Footer
+  }
 };
 </script>
 <style lang="scss"></style>
--- src/components/Header-footer/Header.vue
+++ src/components/Header-footer/Header.vue
@@ -8,59 +8,48 @@
         </template>
       </b-navbar-toggle>
 
-      <b-collapse id="navbar-toggle-collapse" is-nav class="order-1">
-        <b-navbar-nav class="align-items-center py-3 w-100 position-relative ">
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item delay="0" to="about" :left="about" class="about"
-              >About</b-nav-item
-            >
-          </transition>
+      <b-navbar-brand to="/" v-if="slideDown" class="d-md-none">
+        <img src="../../assets/jhr.png" alt="JHR logo" class="img-fluid" width="200px" />
+      </b-navbar-brand>
 
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item to="news" delay=".5" :left="news" class="news"
-              >News</b-nav-item
-            >
-          </transition>
+      <b-collapse id="navbar-toggle-collapse" is-nav class="d-md-flex justify-content-md-between">
+        <transition name="slide-left">
+          <b-navbar-nav class="d-md-flex justify-content-md-around text-center" v-if="slideLeft">
+            <b-nav-item to="about" class="about">About</b-nav-item>
 
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item to="drivers" delay="1" :left="drivers" class="drivers"
-              >Drivers</b-nav-item
-            >
-          </transition>
+            <b-nav-item to="news" class="news">News</b-nav-item>
 
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item
-              to="simulator"
-              delay="1.5"
-              :left="simulator"
-              class="simulator"
-              >Simulator</b-nav-item
-            >
-          </transition>
+            <b-nav-item to="drivers" class="drivers">Drivers</b-nav-item>
+          </b-navbar-nav>
+        </transition>
 
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item to="f3" delay="2" :left="f3" class="f3">F3</b-nav-item>
-          </transition>
+        <transition name="slide">
+          <b-navbar-brand to="/" v-if="slideDown" class="d-none d-md-block">
+            <img src="../../assets/jhr.png" alt="JHR logo" class="img-fluid" width="200px" />
+          </b-navbar-brand>
+        </transition>
 
-          <transition v-on:before-enter="beforeEnter" v-on:enter="enter" appear>
-            <b-nav-item to="f4" delay="2.5" :left="f4" class="f4"
-              >F4</b-nav-item
-            >
-          </transition>
-        </b-navbar-nav>
+        <transition name="slide-right">
+          <b-navbar-nav class="d-md-flex justify-content-md-around text-center" v-if="slideRight">
+            <b-nav-item to="simulator" class="simulator">Simulator</b-nav-item>
+
+            <b-nav-item to="f3" class="f3">F3</b-nav-item>
+
+            <b-nav-item to="f4" class="f4">F4</b-nav-item>
+          </b-navbar-nav>
+        </transition>
 
         <div class="w-100 bg-dark d-md-none">
           <div class="text-center pt-3">
-            <img
-              src="../../assets/jhr.png"
-              alt="JHR logo"
-              class="img-fluid"
-              width="200px"
-            />
+            <img src="../../assets/jhr.png" alt="JHR logo" class="img-fluid" width="200px" />
             <p class="text-secondary">
               Steven Hunter
-              <br />JHR Developments <br />Hunter Park <br />Callywhite Lane
-              <br />Dronfield <br />Derbyshire <br />S18 2XR
+              <br />JHR Developments
+              <br />Hunter Park
+              <br />Callywhite Lane
+              <br />Dronfield
+              <br />Derbyshire
+              <br />S18 2XR
               <br />
             </p>
             <p class="text-secondary">
@@ -107,87 +96,26 @@
           </div>
         </div>
       </b-collapse>
-
-      <transition name="slide">
-        <b-navbar-brand to="/" v-if="show" class="order-0">
-          <img
-            src="../../assets/jhr.png"
-            alt="JHR logo"
-            class="img-fluid"
-            width="200px"
-            delay="0"
-          />
-        </b-navbar-brand>
-      </transition>
     </b-navbar>
   </header>
 </template>
 
 <script>
-import Velocity from "velocity-animate";
-
 export default {
   data() {
     return {
-      windowWidth: 0,
-      about: 0,
-      news: 0,
-      drivers: 0,
-      simulator: 0,
-      f3: 0,
-      f4: 0,
-      show: false,
+      slideDown: false,
+      slideLeft: false,
+      slideRight: false
     };
   },
   name: "Header",
-  methods: {
-    handleResize() {
-      this.windowWidth = window.innerWidth;
-      this.setLeft();
-    },
-    showImage() {
-      this.show = true;
-    },
-    setLeft() {
-      if (this.windowWidth >= 767) {
-        let half = this.windowWidth / 2;
-        let left = half - 200;
-        let right = half - 200;
-        let diff = left / 3;
-
-        // left of logo
-        this.about = diff / 2 - 20;
-        this.news = this.about + diff;
-        this.drivers = this.news + diff;
-
-        // right of logo
-        this.simulator = right + this.about;
-        this.f3 = this.simulator + diff;
-        this.f4 = this.f3 + diff;
-      }
-    },
-    beforeEnter(el) {
-      el.style.left = "0";
-      el.style.opacity = 0;
-    },
-    enter(el, done) {
-      let delay = parseInt(el.getAttribute("delay"));
-      let left = parseInt(el.getAttribute("left"));
-
-      Velocity(
-        el,
-        { opacity: 1, left },
-        { delay, duration: 2000, complete: done }
-      );
-    },
-  },
-  created() {
-    window.addEventListener("resize", this.handleResize);
-    this.handleResize();
-  },
+  methods: {},
   mounted() {
-    this.showImage();
-  },
+    this.slideDown = true;
+    this.slideLeft = true;
+    this.slideRight = true;
+  }
 };
 </script>
 
@@ -219,26 +147,44 @@ ul {
 
 @media (min-width: 768px) {
   #navbar-toggle-collapse {
-    position: absolute;
     width: 100%;
-    margin-bottom: 100px;
-    li {
-      position: relative;
+    ul {
+      width: 50%;
     }
   }
-  .navbar-brand {
-    margin-bottom: 150px;
-    margin-left: auto !important;
-    margin-right: auto !important;
-  }
 }
+// slide-down
 .slide-enter-active {
-  transition: all 2s ease;
+  transition: all 1.5s ease;
 }
 .slide-leave-active {
-  transition: all 2s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 1.5s cubic-bezier(1, 0.5, 0.8, 1);
 }
 .slide-enter, .slide-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(-100px);
+  opacity: 0;
+}
+// slide-left
+.slide-left-enter-active {
+  transition: all 2s ease;
+}
+.slide-left-leave-active {
+  transition: all 2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-left-enter, .slide-left-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(100px);
+  opacity: 0;
+}
+// slide-right
+.slide-right-enter-active {
+  transition: all 2s ease;
+}
+.slide-right-leave-active {
+  transition: all 2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-right-enter, .slide-right-leave-to
 /* .slide-fade-leave-active below version 2.1.8 */ {
   transform: translateX(-100px);
   opacity: 0;
