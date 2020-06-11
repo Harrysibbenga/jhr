4d4669d41694bdf830e92a2ef1ae4e322da0df21
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-11 11:55:38 +0100

    Styled the latest section and added mdb cdn

--- src/components/Latest.vue
+++ src/components/Latest.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="latest">
-    <div class="container-md pt-5">
-      <div class="row">
-        <div class="col-md-6 pt-1 p-0 main px-1 overflow-hidden">
-          <img
-            src="../assets/F4/2 - Donington Park/4ed8bab2-51a0-4df3-aae5-9d570abbe4af.jpg"
-            alt
-            class="main-img"
-          />
+  <div id="latest" class="py-2">
+    <div class="container-md py-5">
+      <div class="row overflow-hidden">
+        <div class="col-md-6 mt-1 p-0 main overflow-hidden">
+          <div class="view overlay main-content px-1 zoom overflow-hidden">
+            <img
+              src="../assets/F4/2 - Donington Park/4ed8bab2-51a0-4df3-aae5-9d570abbe4af.jpg"
+              alt
+              class="main-img"
+            />
+            <div class="mask rgba-black-strong d-flex flex-column justify-content-end pb-5 pl-5">
+              <h2 class="text-white">Headline</h2>
+              <p class="text-white">2 Jan 2020</p>
+              <router-link to="/" class="text-danger">More info</router-link>
+            </div>
+          </div>
         </div>
         <div class="col-md-6 p-0 px-1 d-flex flex-column">
-          <div class="pt-1">
+          <div class="mt-1 view overlay zoom">
             <img
               src="../assets/F4/2 - Donington Park/4ed8bab2-51a0-4df3-aae5-9d570abbe4af.jpg"
               alt
               class="img-fluid"
             />
+            <div class="mask rgba-black-strong d-flex flex-column justify-content-end pb-5 pl-5">
+              <h2 class="text-white">Headline</h2>
+              <p class="text-white">2 Jan 2020</p>
+              <router-link to="/" class="text-danger">More info</router-link>
+            </div>
           </div>
-          <div class="pt-1">
+          <div class="mt-1 view overlay zoom">
             <img
               src="../assets/F4/2 - Donington Park/4ed8bab2-51a0-4df3-aae5-9d570abbe4af.jpg"
               alt
               class="img-fluid"
             />
+            <div class="mask rgba-black-strong d-flex flex-column justify-content-end pb-5 pl-5">
+              <h2 class="text-white">Headline</h2>
+              <p class="text-white">2 Jan 2020</p>
+              <router-link to="/" class="text-danger">More info</router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -48,8 +65,19 @@ export default {
 
 @media (min-width: 768px) {
   .main {
-    .main-img {
-      transform: translateX(-25%);
+    .main-content {
+      transition: all 0.3s ease-in;
+      .main-img {
+        transform: translateX(-25%);
+      }
+    }
+  }
+  .main:hover {
+    .main-content {
+      transform: scale(1.05);
+      .main-img {
+        transform: translateX(-25%);
+      }
     }
   }
 }
