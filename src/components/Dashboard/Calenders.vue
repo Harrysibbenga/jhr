1ef34fe5d4606bcb6a5e5454e6aaa4700a75893a
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-18 10:35:50 +0100

    Added caledar routes

--- src/components/Dashboard/Calenders.vue
+++ src/components/Dashboard/Calenders.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Calenders</div>
+  <div>
+    <b-nav tabs class="pt-3">
+      <b-nav-item to="/calendars/f3">F3 Calendar</b-nav-item>
+      <b-nav-item to="/calendars/f4">F4 Calendar</b-nav-item>
+    </b-nav>
+    <router-view class="pt-3"></router-view>
+  </div>
 </template>
