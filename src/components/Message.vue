ded9116fc19aaa4af6ae555a3c1346f1b68775d2
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-19 11:37:49 +0100

    Created mesage template

---
+++ src/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-if="msg.message != ''"
+    :class="`bg-${msg.type}`"
+    class="pl-5 mt-2 text-white text-center"
+  >
+    <p>{{ msg.message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+};
+</script>
