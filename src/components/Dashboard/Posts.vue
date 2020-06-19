f18bc55956643aca7c7f3af245d6798341be4c96
Harry Sibbenga <sibbengaharry@gmail.com>
2020-06-19 11:38:44 +0100

    Create edit delet finished, added modals for each, added ckeditor

--- src/components/Dashboard/Posts.vue
+++ src/components/Dashboard/Posts.vue
@@ -6,10 +6,34 @@
         :togglers="1"
         :toggleClass="['btn btn-primary']"
         :toggleText="['Add Post']"
+        class="h-auto"
       >
-        <form @submit.prevent="submitForm" class="p-2">
-          <div class="md-form col-md-6 col-lg-4 p-0">
-            <mdb-input type="date" inline v-model.trim="post.date" />
+        <div style="height:400px" class="text-center pt-2">
+          <img
+            v-if="post.url"
+            :src="post.url"
+            :alt="img.alt"
+            class="img-fluid"
+          />
+
+          <img
+            v-else
+            src="../../assets/img-placeholder.png"
+            alt="Placeholder image"
+            class="img-fluid"
+          />
+        </div>
+        <form @submit.prevent="submitForm" class="p-2 pt-5">
+          <div class="row">
+            <div class="md-form col-4 p-0">
+              <mdb-btn color="primary" inline @click="uploadImage = true"
+                >Headline Image</mdb-btn
+              >
+            </div>
+
+            <div class="md-form col-md-6 col-lg-4">
+              <mdb-input type="date" inline v-model.trim="post.date" />
+            </div>
           </div>
 
           <div class="md-form">
@@ -20,38 +44,13 @@
             <mdb-input label="Excerpt" inline v-model.trim="post.excerpt" />
           </div>
 
-          <div class="md-form">
-            <mdb-input
-              label="Content"
-              type="textarea"
-              v-model="post.content"
-              :rows="5"
-            ></mdb-input>
+          <div class="md-form" style="height:500px">
+            <app-editor
+              @content-updated="newContent($event)"
+              :content="post.content"
+            ></app-editor>
+            <mdb-btn color="none" type="submit">Add Post</mdb-btn>
           </div>
-
-          <div class="row col-12 col-md-6 col-lg-4 py-3">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="imageInput">Upload</span>
-              </div>
-              <div class="custom-file">
-                <input
-                  type="file"
-                  class="custom-file-input"
-                  id="imageInput"
-                  aria-describedby="imageInput"
-                  @change="checkFile($event)"
-                  ref="imageInput"
-                  accept="image/jpeg image/png"
-                />
-                <label class="custom-file-label" for="inputGroupFile01"
-                  >Header image</label
-                >
-              </div>
-            </div>
-          </div>
-
-          <mdb-btn color="none" type="submit">Add Post</mdb-btn>
         </form>
       </mdb-collapse>
     </mdb-container>
@@ -68,7 +67,7 @@
       </transition>
     </mdb-container>
 
-    <div class="container-fluid pt-5 p-0">
+    <div class="container-fluid mt-5 p-0">
       <table class="table">
         <thead class="black text-white">
           <tr>
@@ -85,7 +84,7 @@
               <a @click="editPost(post)" class="mr-1">
                 <mdb-icon icon="edit" />
               </a>
-              <a @click="deletePost(post.id)" class="ml-1">
+              <a @click="deletePost(post)" class="ml-1">
                 <mdb-icon icon="trash-alt" />
               </a>
             </td>
@@ -109,13 +108,185 @@
         </mdb-btn>
       </div>
     </div>
+    <!-- editModel  -->
+    <mdb-modal
+      top
+      position="top"
+      fullHeight
+      direction="top"
+      :show="editModal"
+      @close="cancelEdit"
+    >
+      <mdb-modal-header>
+        <mdb-modal-title>{{ clickedPost.title }}</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>
+        <form @submit.prevent class="p-2">
+          <img
+            v-if="clickedPost.url"
+            :src="clickedPost.url"
+            class="img-fluid"
+          />
+
+          <div class="md-form col-md-6 col-lg-4 p-0">
+            <mdb-input type="date" inline v-model.trim="clickedPost.date" />
+          </div>
+
+          <div class="md-form">
+            <mdb-input label="Title" inline v-model.trim="clickedPost.title" />
+          </div>
+
+          <div class="md-form">
+            <mdb-input
+              label="Excerpt"
+              inline
+              v-model.trim="clickedPost.excerpt"
+            />
+          </div>
+
+          <div class="md-form">
+            <app-editor
+              @content-updated="editContent($event)"
+              :content="clickedPost.content"
+            ></app-editor>
+          </div>
+        </form>
+      </mdb-modal-body>
+      <mdb-container>
+        <transition name="fade">
+          <div
+            v-if="editMsg.message != ''"
+            :class="`bg-${editMsg.type}`"
+            class="pl-5 mt-2 text-white text-center"
+          >
+            <p>{{ editMsg.message }}</p>
+          </div>
+        </transition>
+      </mdb-container>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" @click.native="cancelEdit">Close</mdb-btn>
+        <mdb-btn color="primary" @click="saveEdit">Save changes</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+
+    <!-- deleteModal -->
+    <mdb-modal
+      frame
+      position="top"
+      direction="top"
+      :show="deleteModal"
+      @close="deleteModal = false"
+    >
+      <mdb-modal-body class="text-center">
+        <span class="text-danger"
+          >Are you sure you want to delete this post</span
+        >
+        {{ toDeletePost.title }}
+        <mdb-btn color="secondary" @click.native="cancelDelete">Close</mdb-btn>
+        <mdb-btn color="danger" @click="confirmDelete">Delete</mdb-btn>
+      </mdb-modal-body>
+    </mdb-modal>
+
+    <!-- existsModal -->
+    <mdb-modal size="md" :show="existsModal" @close="existsModal = false">
+      <mdb-modal-header>
+        <mdb-modal-title>Image exists with that name</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>
+        <div class="row col-12 col-md-6 col-lg-4 py-3">
+          <p>Would you like to use this one ?</p>
+        </div>
+        <img :src="img.content.url" :alt="img.content.alt" class="img-fluid" />
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" size="sm" @click.native="declineUse"
+          >No</mdb-btn
+        >
+        <mdb-btn color="primary" size="sm" @click="confirmUse">Yes</mdb-btn>
+      </mdb-modal-footer>
+    </mdb-modal>
+
+    <!-- uploadImage -->
+    <mdb-modal size="md" :show="uploadImage" @close="uploadImage = false">
+      <mdb-modal-header>
+        <mdb-modal-title>Upload image</mdb-modal-title>
+      </mdb-modal-header>
+      <mdb-modal-body>
+        <div class="row">
+          <div class="col-12">
+            <div class="md-form">
+              <mdb-input label="Description" inline v-model.trim="img.alt" />
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="imageInput">Upload</span>
+              </div>
+              <div class="custom-file">
+                <input
+                  type="file"
+                  class="custom-file-input"
+                  id="imageInput"
+                  aria-describedby="imageInput"
+                  @change="checkFile($event)"
+                  ref="imageInput"
+                  accept="image/jpeg image/png"
+                />
+                <label
+                  class="custom-file-label"
+                  for="inputGroupFile01"
+                  v-if="file.name"
+                  >{{ file.name }}</label
+                >
+                <label class="custom-file-label" for="inputGroupFile01" v-else
+                  >Headline image</label
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </mdb-modal-body>
+      <mdb-modal-footer>
+        <mdb-btn color="secondary" size="sm" @click.native="uploadImage = false"
+          >Close</mdb-btn
+        >
+        <mdb-btn
+          color="primary"
+          size="sm"
+          :disabled="img.alt == ''"
+          @click="saveFile"
+          >Save</mdb-btn
+        >
+      </mdb-modal-footer>
+      <transition name="fade">
+        <div
+          v-if="uploadMsg.message != ''"
+          :class="`bg-${uploadMsg.type}`"
+          class="text-white text-center"
+        >
+          <p>{{ uploadMsg.message }}</p>
+        </div>
+      </transition>
+    </mdb-modal>
   </div>
 </template>
 
 <script>
 import moment from "moment";
-import { postsCollection } from "../../../firebase";
-import { mdbInput, mdbBtn, mdbCollapse, mdbContainer, mdbIcon } from "mdbvue";
+import { postsCollection, imageCollection } from "../../../firebase";
+import {
+  mdbInput,
+  mdbBtn,
+  mdbCollapse,
+  mdbContainer,
+  mdbIcon,
+  mdbModalHeader,
+  mdbModalTitle,
+  mdbModalBody,
+  mdbModalFooter,
+  mdbModal,
+} from "mdbvue";
 
 export default {
   data() {
@@ -128,10 +299,38 @@ export default {
         date: "",
         content: "",
         year: "",
+        imgId: "",
+        url: "",
       },
-      search: "",
       pageNumber: 0,
       msg: "",
+      editMsg: "",
+      editModal: false,
+      deleteModal: false,
+      changeModel: false,
+      toDeletePost: {
+        id: "",
+        title: "",
+      },
+      clickedPost: {
+        id: "",
+        title: "",
+        desc: "",
+        excerpt: "",
+        slug: "",
+        date: "",
+        content: "",
+        year: "",
+      },
+      existsModal: false,
+      uploadImage: false,
+      file: "",
+      img: {
+        id: "",
+        content: "",
+        alt: "",
+      },
+      confirmUpdate: false,
     };
   },
   props: {
@@ -147,10 +346,21 @@ export default {
     mdbCollapse,
     mdbContainer,
     mdbIcon,
+    mdbModalHeader,
+    mdbModalTitle,
+    mdbModalBody,
+    mdbModalFooter,
+    mdbModal,
   },
   computed: {
     posts() {
       return this.$store.getters["posts/getPosts"];
+    },
+    uploadMsg() {
+      return this.$store.getters["images/getMsg"];
+    },
+    updatedImage() {
+      return this.$store.getters["images/getImage"];
     },
     pageCount() {
       let l = this.posts.length,
@@ -164,6 +374,33 @@ export default {
     },
   },
   methods: {
+    newContent(event) {
+      this.post.content = event.content;
+      this.confirmUpdate = event.clicked;
+    },
+    editContent(event) {
+      this.clickedPost.content = event.content;
+      this.confirmUpdate = event.clicked;
+    },
+    reset() {
+      this.post = {
+        title: "",
+        desc: "",
+        excerpt: "",
+        slug: "",
+        date: "",
+        content: "",
+        year: "",
+        imgId: "",
+        url: "",
+      };
+      this.file = "";
+      this.img = {
+        id: "",
+        content: "",
+        alt: "",
+      };
+    },
     addPost() {
       let slugArry = [],
         newSlug = [],
@@ -189,17 +426,11 @@ export default {
           content: this.post.content,
           year: this.post.year,
           createdOn: new Date(),
+          imgId: this.post.imgId,
+          url: this.post.url,
         })
         .then(() => {
-          this.post = {
-            title: "",
-            desc: "",
-            excerpt: "",
-            slug: "",
-            date: "",
-            content: "",
-            year: "",
-          };
+          this.reset();
           this.msg = {
             type: "success",
             message: "Post added",
@@ -225,7 +456,48 @@ export default {
         });
     },
     checkFile(event) {
-      console.log(event);
+      this.file = event.target.files[0];
+      imageCollection
+        .where("name", "==", this.file.name)
+        .get()
+        .then((docs) => {
+          docs.forEach((doc) => {
+            if (doc.exists) {
+              this.existsModal = true;
+              this.uploadImage = false;
+              this.img.content = doc.data();
+              this.img.id = doc.id;
+              return;
+            }
+          });
+        });
+    },
+    saveFile() {
+      let payload = {};
+      payload.file = this.file;
+      payload.alt = this.img.alt;
+      this.$store.dispatch("images/uploadImage", payload);
+      setTimeout(() => {
+        this.img.content = this.updatedImage;
+        this.post.imgId = this.img.content.id;
+        this.post.url = this.img.content.url;
+        this.file.name = "";
+        this.uploadImage = false;
+      }, 2000);
+    },
+    confirmUse() {
+      this.post.imgId = this.img.id;
+      this.existsModal = false;
+      this.post.url = this.img.content.url;
+      this.img.alt = this.img.content.alt;
+      this.file.name = "";
+    },
+    declineUse() {
+      this.existsModal = false;
+      this.post.url = "";
+      this.img.alt = "";
+      this.post.imgId = "";
+      this.file.name = "";
     },
     submitForm() {
       this.addPost();
@@ -236,11 +508,24 @@ export default {
     prevPage() {
       this.pageNumber--;
     },
-    deletePost(id) {
+    deletePost(post) {
+      this.deleteModal = true;
+      this.toDeletePost.title = post.title;
+      this.toDeletePost.id = post.id;
+    },
+    cancelDelete() {
+      this.toDeletePost = {
+        id: "",
+        title: "",
+      };
+      this.deleteModal = false;
+    },
+    confirmDelete() {
       postsCollection
-        .doc(id)
+        .doc(this.toDeletePost.id)
         .delete()
         .then(() => {
+          this.deleteModal = false;
           this.msg = {
             type: "danger",
             message: "Post deleted",
@@ -266,7 +551,88 @@ export default {
         });
     },
     editPost(post) {
-      console.log(post);
+      this.editModal = true;
+      this.clickedPost.id = post.id;
+      this.clickedPost.title = post.title;
+      this.clickedPost.content = post.content;
+      this.clickedPost.desc = post.desc;
+      this.clickedPost.excerpt = post.excerpt;
+      this.clickedPost.slug = post.slug;
+      this.clickedPost.date = post.date;
+      this.clickedPost.content = post.content;
+      this.clickedPost.year = post.year;
+      this.clickedPost.url = post.url;
+      this.clickedPost.imgId = post.imgId;
+    },
+    cancelEdit() {
+      this.clickedPost = {
+        id: "",
+        title: "",
+        desc: "",
+        excerpt: "",
+        slug: "",
+        date: "",
+        content: "",
+        year: "",
+        url: "",
+        imgId: "",
+      };
+      this.editModal = false;
+    },
+    saveEdit() {
+      let slugArry = [],
+        newSlug = [],
+        date = "";
+      // create slug
+      slugArry = this.clickedPost.title.split(" ");
+      date = this.clickedPost.date;
+      this.clickedPost.year = date.split("-")[0];
+
+      slugArry.forEach((item) => {
+        newSlug.push(item.toLowerCase());
+      });
+
+      this.clickedPost.slug = newSlug.join("-") + "-" + date;
+
+      postsCollection
+        .doc(this.clickedPost.id)
+        .update({
+          title: this.clickedPost.title,
+          desc: this.clickedPost.desc,
+          excerpt: this.clickedPost.excerpt,
+          slug: this.clickedPost.slug,
+          date: this.clickedPost.date,
+          content: this.clickedPost.content,
+          year: this.clickedPost.year,
+          createdOn: new Date(),
+          imgId: this.clickedPost.imgId,
+          url: this.clickedPost.url,
+        })
+        .then(() => {
+          this.cancelEdit();
+          this.msg = {
+            type: "success",
+            message: "Post updated",
+          };
+          setTimeout(() => {
+            this.msg = {
+              type: "",
+              message: "",
+            };
+          }, 2000);
+        })
+        .catch((err) => {
+          this.editMsg = {
+            type: "warning",
+            message: err.message,
+          };
+          setTimeout(() => {
+            this.editMsg = {
+              type: "",
+              message: "",
+            };
+          }, 2000);
+        });
     },
   },
   filters: {
@@ -280,3 +646,5 @@ export default {
   },
 };
 </script>
+
+<style lang="css"></style>
