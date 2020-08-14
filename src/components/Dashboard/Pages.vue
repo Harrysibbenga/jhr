344b733a2b1057a77f30fce2bee6b1a9498d2676
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:32:29 +0100

    Created pages tabs

---
+++ src/components/Dashboard/Pages.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="container">
+      <b-nav tabs class="pt-3">
+        <b-nav-item to="/dashboard/pages/">About</b-nav-item>
+        <b-nav-item to="/dashboard/pages/simulator">Simulator</b-nav-item>
+        <b-nav-item to="/dashboard/pages/f3">F3</b-nav-item>
+        <b-nav-item to="/dashboard/pages/f4">F4</b-nav-item>
+      </b-nav>
+    </div>
+    <router-view class="pt-3"></router-view>
+  </div>
+</template>
