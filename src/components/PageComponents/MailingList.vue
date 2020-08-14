48388c8ab539c8b2e163a00eaedf23d73a4254cc
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:36:53 +0100

    Created mailing list section

---
+++ src/components/PageComponents/MailingList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex flex-column flex-center bg-dark py-5">
+    <h3 class="text-light">Keep up to date with our latest content</h3>
+    <button class="btn btn-secondary" @click="open()">Subscribe</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MailingList",
+  methods: {
+    open() {
+      this.$store.dispatch("global/setModal", true);
+    },
+  },
+};
+</script>
