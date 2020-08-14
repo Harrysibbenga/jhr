169b83856f5192f2b9c3c5026de339d5da104085
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:32:08 +0100

    Made sim page dynamic and editable throough dashboard

--- src/views/Simulator.vue
+++ src/views/Simulator.vue
@@ -5,18 +5,25 @@
     <div id="about" class="container-fluid">
       <div class="row">
         <div class="col-12 col-md-6 offset-md-3 offset-lg-0 col-lg-4 p-5 px-md-0 py-md-5 p-lg-5">
-          <img src="../assets/Sim.png" alt class="img-fluid" />
+          <img :src="sim.url" class="img-fluid" :alt="sim.alt" />
         </div>
         <div class="col-12 col-lg-8 px-md-5 p-lg-5">
           <div class="pb-5">
-            <h3 class="pb-2">JHR Developments race simulator</h3>
-            <p>Emphasising its commitment to the development of young drivers that come through its doors, JHR Developments has its own bespoke, in-house, state-of-the-art race simulator for use by its drivers as much as they require.</p>
-            <p>The simulator can be configured to a variety of cars, which not only includes the F4 British Championship and BRDC British F3 Championship cars the team will compete with in 2019, but also a variety of other machines.</p>
-            <p>Replicating an almost 1:1 feel in its steering, utilising hydraulic brakes and a gas spring throttle, and with precise gearing, the simulator enables drivers to learn circuits, get to grips with new cars, and refine their technique, all without the additional costs and restrictions of physically testing on track.</p>
+            <div class="col-12 pt-3 text-justify" v-html="sim.content"></div>
           </div>
-          <contact></contact>
+          <contact-modal></contact-modal>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    sim() {
+      return this.$store.getters["sim/getContent"];
+    },
+  },
+};
+</script>
