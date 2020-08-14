a7ff0e7ee3dfd5fab5e722d5c9519cca490d64fd
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:44:18 +0100

    Created dynamic f3 and f4 views

--- src/views/F3.vue
+++ src/views/F3.vue
@@ -3,7 +3,7 @@
     <Header></Header>
     <div id="about" class="container-fluid">
       <div class="row">
-        <div class="col-12 col-md-4 p-5 order-md-1 text-center">
+        <div class="col-12 col-lg-4 p-5 order-1 text-center">
           <h3 class="mx-5">Calendar</h3>
           <div class="d-flex flex-column px-5 pt-1">
             <div v-for="(fixture,index) in fixtures" :key="index">
@@ -12,24 +12,18 @@
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-8 bg-white p-md-5 order-md-0">
+        <div class="col-12 col-lg-8 bg-white p-md-5 order-0">
+          <img :src="content.img_1.url" class="img-fluid" :alt="content.img_1.alt" />
           <div class="p-5">
-            <h3 class="pb-4">BRDC British F3 Championship:</h3>
-            <p
-              class="mt-3"
-            >The BRDC British F3 Championship returns in 2019 with 24 races across eight events, seven of which remain co-headliners with British GT. Each weekend features a 30-minute qualifying session and three 20-minute races, with the grid for race one set by qualifying order, race two by a true-reverse grid, and race three from the fastest laps from the first two contests.</p>
-            <p
-              class="mt-3"
-            >Points are scored from 35 for a win, to one for 20th position for races one and three, while in the reverse grid race two, points are scored from 20 for a win to one for 15th place. In addition, points in this race are also awarded for positions gained, providing an opportunity for talented drivers to make a real difference.</p>
-            <p
-              class="mt-3"
-            >The Championship races with the Tatuus F4-016 car, which is powered by a 230bhp Cosworth engine. An improved aerodynamic set-up includes a highly adjustable front-wing and two-piece rear-wing, and an F1-style front splitter and rear diffuser.</p>
-            <p
-              class="mt-3"
-            >The car features a six-speed sequential gearbox with paddle shift provided by Sadev and is fitted with a limited-slip differential. Drivers race on Pirelli tyres, with brakes provided by AP Racing. The twin wishbone suspension with two-way adjustable dampers and adjustable front and rear anti-roll bars provides drivers with plenty of scope for set-up changes.</p>
-            <p
-              class="mt-3"
-            >BRDC F3 has a media partnership with world-leading motorsport publication Autosport, providing unrivalled media coverage not only of race weekends, but also with regular news features and updates. Championship graduates include 2019 Formula 1 World Championship drivers Lando Norris and George Russell, and 2018 FIA Formula 3 European Championship race-winners Dan Ticktum and Enaam Ahmed.</p>
+            <div class="col-12 pt-3 text-justify" v-html="content.content"></div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <img :src="content.img_2.url" class="img-fluid" :alt="content.img_2.alt" />
+            </div>
+            <div class="col-6">
+              <img :src="content.img_3.url" class="img-fluid" :alt="content.img_3.alt" />
+            </div>
           </div>
         </div>
       </div>
@@ -39,11 +33,18 @@
 
 <script>
 import moment from "moment";
+import { mdbSticky } from "mdbvue";
 export default {
+  directives: {
+    sticky: mdbSticky,
+  },
   computed: {
     fixtures() {
       return this.$store.getters["formula3/getFixtures"];
-    }
+    },
+    content() {
+      return this.$store.getters["f3page/getContent"];
+    },
   },
   filters: {
     formatDate(val) {
@@ -52,7 +53,7 @@ export default {
       }
       let date = val;
       return moment(date).format("Do MMM YYYY");
-    }
-  }
+    },
+  },
 };
 </script>
--- src/views/F4.vue
+++ src/views/F4.vue
@@ -3,7 +3,7 @@
     <Header></Header>
     <div id="about" class="container-fluid">
       <div class="row">
-        <div class="col-12 col-md-4 p-5 order-md-1 text-center">
+        <div class="col-12 col-lg-4 p-5 order-1 text-center">
           <h3 class="mx-5">Calendar</h3>
           <div class="d-flex flex-column px-5 pt-1">
             <div v-for="(fixture,index) in fixtures" :key="index">
@@ -12,30 +12,18 @@
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-8 bg-white p-md-5 order-md-0">
+        <div class="col-12 col-lg-8 bg-white p-md-5 order-0">
+          <img :src="content.img_1.url" class="img-fluid" :alt="content.img_1.alt" />
           <div class="p-5">
-            <h3 class="pb-4">F4 British Championship:</h3>
-            <p
-              class="mt-3"
-            >The F4 British Championship continues the rich heritage of junior single-seater racing in the United Kingdom, helping young drivers develop the skills and abilities that they’ll need to progress to the highest levels of motorsport.</p>
-            <p
-              class="mt-3"
-            >Run to FIA Formula 4 regulations, the series has support from the FIA, and features cars boasting the latest safety standards in a cost-controlled environment where anyone can succeed – not just those with the biggest budgets.</p>
-            <p
-              class="mt-3"
-            >It is the only championship in the UK which is open to 15-year olds, and has strict engineering standards, with control tyres issued to teams. Data is available to drivers and shared each weekend, with development overseen by the MSA.</p>
-            <p
-              class="mt-3"
-            >The championship winner is invited to the FIA prize-giving ceremony and MSA night of champions, while the top seven drivers score FIA Super License points – an important step as they hope to progress to a career in Formula 1.</p>
-            <p
-              class="mt-3"
-            >Races are run on the popular BTCC package, with live television coverage in the UK and free online streaming. The programs are available on demand for 28 days after each broadcast, and the total UK TV audience is a staggering 17.2 million.</p>
-            <p
-              class="mt-3"
-            >Some 385,000 fans pack the circuits across the country, while the official British F4 podcast received 118,000 downloads in 2016 alone. The official Facebook page has 16,000 likes, and Twitter 9,000 followers.</p>
-            <p
-              class="mt-3"
-            >Each weekend features two 45-minute test sessions, one 20-minute qualifying period, and three 20-minute races; a total of 170 minutes of track time</p>
+            <div class="col-12 pt-3 text-justify" v-html="content.content"></div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <img :src="content.img_2.url" class="img-fluid" :alt="content.img_2.alt" />
+            </div>
+            <div class="col-6">
+              <img :src="content.img_3.url" class="img-fluid" :alt="content.img_3.alt" />
+            </div>
           </div>
         </div>
       </div>
@@ -49,7 +37,10 @@ export default {
   computed: {
     fixtures() {
       return this.$store.getters["formula4/getFixtures"];
-    }
+    },
+    content() {
+      return this.$store.getters["f4page/getContent"];
+    },
   },
   filters: {
     formatDate(val) {
@@ -58,7 +49,7 @@ export default {
       }
       let date = val;
       return moment(date).format("Do MMM YYYY");
-    }
-  }
+    },
+  },
 };
 </script>
