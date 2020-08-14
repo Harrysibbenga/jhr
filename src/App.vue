c5e88a270cbe4a5d33e7792b371052a14aaa21e6
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:29:39 +0100

    Added maling Model

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <loader></loader>
     <router-view />
+    <mailing-model></mailing-model>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Footer from "@/components/Header-footer/Footer";
+import MailingModel from "@/components/Modals/MailingListModal";
 
 export default {
   components: {
     Footer,
+    MailingModel,
   },
   created() {
     this.$store.dispatch("posts/setPosts");
@@ -21,6 +24,10 @@ export default {
     this.$store.dispatch("circuits/setCircuits");
     this.$store.dispatch("images/setImages");
     this.$store.dispatch("carousel/setImages");
+    this.$store.dispatch("about/setContent");
+    this.$store.dispatch("sim/setContent");
+    this.$store.dispatch("f3page/setContent");
+    this.$store.dispatch("f4page/setContent");
   },
 };
 </script>
