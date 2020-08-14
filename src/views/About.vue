7c1cc0959d21eab71d05ccce6cd183c9030c1176
Harry Sibbenga <sibbengaharry@gmail.com>
2020-08-14 09:43:37 +0100

    Created a dynamic about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,58 +5,25 @@
     <div id="about" class="container-fluid">
       <div class="row">
         <div class="col-12 col-md-6 offset-md-3 offset-lg-0 col-lg-4 p-5 px-md-0 py-md-5 p-lg-5">
-          <img src="../assets/About.png" alt class="img-fluid" />
+          <img :src="about.url" class="img-fluid" :alt="about.alt" />
         </div>
         <div class="col-12 col-lg-8 px-md-5 p-lg-5">
           <div class="pb-5">
-            <h3 class="pb-2">About</h3>
-            <p>
-              JHR Developments has been in operation since 1995, racing in a
-              number of single-seater and sportscar series. Originally set up to
-              help the late Jamie Hunter compete, the team is run by Jamie’s
-              brother Steven in his memory, after his tragic death in 2000.
-            </p>
-            <p>
-              The team has raced in European Formula Renault, F4 British
-              Championship, Renault UK Clio Cup, Britcar, Seat Cupra Championship,
-              and Porsche Carrera Cup GB, with a number of well-known drivers
-              coming through the doors.
-            </p>
-            <p>
-              For 2019 JHR Developments will return to the F4 British Championship
-              while making its debut in the BRDC British F3 Championship. This
-              will provide drivers with a logical progression route in their
-              single-seater careers as they look to prove themselves in two of the
-              toughest series on the planet.
-            </p>
+            <div class="col-12 pt-3 text-justify" v-html="about.content"></div>
           </div>
-          <div>
-            <h3 class="pb-2">WHY CHOSE JHR Developments?</h3>
-            <p>
-              JHR is the only team to have competed in the TOCA package every year
-              since it began
-            </p>
-            <p>
-              JHR has achieved more race victories and championship wins than any
-              other team on the TOCA package
-            </p>
-            <p>
-              In its quest to win, JHR employs experienced and successful personal
-              to head up all departments
-            </p>
-            <p>
-              Equality. All drivers in the team enjoy equal status with data and
-              video shared not only from the present but from previous race
-              winning drives, presented by an experienced driver and data coach
-            </p>
-            <p>
-              The team has its own, in-house, state-of-the-art race simulator for
-              use by its drivers as much as they require.
-            </p>
-          </div>
-          <contact></contact>
+          <contact-modal></contact-modal>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    about() {
+      return this.$store.getters["about/getContent"];
+    },
+  },
+};
+</script>
