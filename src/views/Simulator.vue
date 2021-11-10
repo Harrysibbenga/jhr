6a8b5cb76c54de4a30ffd10a456803f3465d41bd
hs <sibbengaharry@gmail.com>
2021-11-10 12:36:03 +0000

    Added controls to the video

--- src/views/Simulator.vue
+++ src/views/Simulator.vue
@@ -18,6 +18,7 @@
               width="100%"
               height="100%"
               class="position-relative"
+              controls
           >
               <source :src="sim.displayVid.url" type="video/mp4">
               Your browser does not support the video tag.
